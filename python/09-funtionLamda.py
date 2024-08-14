a4ba23e1a3802db5dd34d5a33459f6b30bf15deb
Jhonni10 <jhonieripianoscue@gmail.com>
2024-08-14 16:53:06 -0500

    less units

---
+++ python/09-funtionLamda.py
@@ -0,0 +1,11 @@
+square = lambda x: x * x
+
+people = [
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Cho", "house": "Ravenclaw"},
+    {"name": "Draco", "house": "Slytherin"}
+]
+
+people.sort(key=lambda person: person["name"])
+
+print(people)
---
+++ python/funtions.py
@@ -0,0 +1,7 @@
+def square(x):
+    return x**2
+
+numbers = range(10);
+
+for i in numbers:
+    print(f"The square of {i} is {square(i)}")
