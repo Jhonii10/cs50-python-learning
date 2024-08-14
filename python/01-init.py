86f4bbef55901dba0a50f4a3867f558dcac32e49
Jhonni10 <jhonieripianoscue@gmail.com>
2024-08-14 14:43:57 -0500

    first units

---
+++ python/01-init.py
@@ -0,0 +1,20 @@
+# hello word 
+
+print("Hello, world!")
+
+# variables 
+
+a = 28
+b = 1.5
+c = "Hello!"
+d = True
+e = None
+
+name = input("Name: ")
+print("Hello, " + name)
+
+# formatos de cadena 
+
+print(f"hello , {input('nombre:')}")
+
+
